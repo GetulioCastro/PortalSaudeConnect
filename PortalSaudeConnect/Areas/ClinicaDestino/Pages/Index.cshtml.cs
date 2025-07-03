daa4cff83b2160eda0f5556e28e10b42d3fc111b
Getulio <augustoge71@gmail.com>
2025-07-03 11:55:21 -0300

    Commit inicial, primeiros arquivos e alterações do Portal MVP criados

---
+++ PortalSaudeConnect/Areas/ClinicaDestino/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortalSaudeConnect.Areas.ClinicaDestino.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
